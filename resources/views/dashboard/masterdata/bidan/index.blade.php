5ea5c7e7089c7d51903dfd06880753da4cfa1a92
hawkxc <hawkxc068@gmail.com>
2025-05-05 23:05:57 +0700

    add : masterdata > bidan view

--- resources/views/dashboard/masterdata/bidan/index.blade.php
+++ resources/views/dashboard/masterdata/bidan/index.blade.php
@@ -1,5 +1,20 @@
-@vite('resources/js/app.js')
-
-<body>
-    <h1>Bidan</h1>
-</body>
+<x-app-layout>
+    <div>
+        <div class="text-sm breadcrumbs">
+            <ul class="font-light">
+                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                <li><a href="#">Masterdata</a></li>
+                <li>Bidan</li>
+            </ul>
+        </div>
+        <div class="flex flex-col mx-auto bg-white rounded-sm shadow-sm">
+            <div class="flex flex-row items-center justify-between w-full p-3 align-middle bg-white">
+                <h3 class="flex-1 text-xl">Masterdata Bidan</h3>
+                <button class="btn btn-sm btn-primary"><i class="ri-add-line"></i> Tambah Baru</button>
+            </div>
+            <hr />
+            <div class="p-3 bg-white">
+            </div>
+        </div>
+    </div>
+</x-app-layout>

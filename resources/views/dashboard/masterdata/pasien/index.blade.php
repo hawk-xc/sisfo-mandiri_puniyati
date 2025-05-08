41469515874747bc67c96de0bc5ddf960abd2635
hawkxc <hawkxc068@gmail.com>
2025-05-08 22:58:05 +0700

    update : add pasien index view

--- resources/views/dashboard/masterdata/pasien/index.blade.php
+++ resources/views/dashboard/masterdata/pasien/index.blade.php
@@ -1,5 +1,29 @@
-@vite('resources/js/app.js')
+<x-app-layout>
+    <div class="p-6">
+        <div class="shadow-xl card bg-base-100">
+            <div class="card-body">
+                <div id="head" class="flex flex-row justify-between">
+                    <h2 class="mb-6 text-2xl font-bold card-title">Data Pasien</h2>
+                    <a href="{{ route('pasien.create') }}" class="btn btn-primary">
+                        <i class="ri-add-line"></i> Tambah data
+                    </a>
+                </div>
+                {{ $dataTable->table(['class' => 'w-full table table-zebra mt-2 hover:table-auto']) }}
+            </div>
+        </div>
+    </div>
 
-<body>
-    <h1>Pasien</h1>
-</body>
+    @push('styles')
+        <link rel="stylesheet" href="{{ asset('assets/plugins/datatable/css/jquery.dataTables.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}">
+    @endpush
+
+    @push('scripts')
+        <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+        <script src={{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}></script>
+        <script src={{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}></script>
+        <script src={{ asset('assets/plugins/datatable/js/dataTables.dataTables.min.js') }}></script>
+        <script src={{ asset('assets/plugins/datatable/js/dataTables.responsive.min.js') }}></script>
+        {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+    @endpush
+</x-app-layout>

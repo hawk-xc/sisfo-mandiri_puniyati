ca3fb3443bb32318e8f7f27f6c85d2438dea0c68
hawkxc <hawkxc068@gmail.com>
2025-05-10 12:14:58 +0700

    add : add obat model

---
+++ app/Models/Obat.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Obat extends Model
+{
+    protected $table = 'obat';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = (string) \Illuminate\Support\Str::uuid();
+        });
+    }
+
+    protected $fillable = [
+        'nama',
+        'jenis',
+        'stok',
+        'harga_beli',
+        'harga_jual'
+    ];
+}

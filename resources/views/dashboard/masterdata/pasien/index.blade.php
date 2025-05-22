8977f0bdc44724e4b4bf6f53a788c3ff19ab12fa
hawkxc <hawkxc068@gmail.com>
2025-05-22 10:12:01 +0700

    update : update masterdata views -> add export pdf button

--- resources/views/dashboard/masterdata/pasien/index.blade.php
+++ resources/views/dashboard/masterdata/pasien/index.blade.php
@@ -7,6 +7,9 @@
                     {{-- <a href="{{ route('pasien.create') }}" class="btn btn-primary">
                         <i class="ri-add-line"></i> Tambah data
                     </a> --}}
+                    <button onclick="exportData('pasien', 'pdf')" class="btn btn-outline btn-neutral btn-sm">
+                        <i class="ri-file-pdf-2-line"></i> Export PDF
+                    </button>
                 </div>
                 {{ $dataTable->table(['class' => 'w-full table table-zebra mt-2 hover:table-auto']) }}
             </div>
@@ -25,5 +28,20 @@
         <script src={{ asset('assets/plugins/datatable/js/dataTables.dataTables.min.js') }}></script>
         <script src={{ asset('assets/plugins/datatable/js/dataTables.responsive.min.js') }}></script>
         {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+
+        <script>
+            function exportData(dataType, exportType) {
+                const urlParams = new URLSearchParams(window.location.search);
+
+                let url = `/dashboard/export/export/${dataType}?export_type=${exportType}`;
+
+                if (urlParams.has('sort')) {
+                    url += `&sort=${urlParams.get('sort')}`;
+                }
+
+                console.log('Export URL:', url);
+                window.location.href = url;
+            }
+        </script>
     @endpush
 </x-app-layout>

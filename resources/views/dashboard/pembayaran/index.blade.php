b770e228b065e09ece0d2aa36f840a034f6286ef
hawkxc <hawkxc068@gmail.com>
2025-05-22 16:46:53 +0700

    add : update and add new views

--- resources/views/dashboard/pembayaran/index.blade.php
+++ resources/views/dashboard/pembayaran/index.blade.php
@@ -3,10 +3,10 @@
         <div class="shadow-xl card bg-base-100">
             <div class="card-body">
                 <div id="head" class="flex flex-row justify-between">
-                    <h2 class="mb-6 text-2xl font-bold card-title">Data Pemeriksaan Pasien</h2>
-                    <a href="{{ route('pemeriksaan.create') }}" class="btn btn-primary">
-                        <i class="ri-add-line"></i> Tambah data
-                    </a>
+                    <h2 class="mb-6 text-2xl font-bold card-title">Data Pembayaran</h2>
+                    <button onclick="exportData('pembayaran', 'pdf')" class="btn btn-outline btn-neutral btn-sm">
+                        <i class="ri-file-pdf-2-line"></i> Export PDF
+                    </button>
                 </div>
                 {{ $dataTable->table(['class' => 'w-full table table-zebra mt-2 hover:table-auto']) }}
             </div>
@@ -25,5 +25,20 @@
         <script src={{ asset('assets/plugins/datatable/js/dataTables.dataTables.min.js') }}></script>
         <script src={{ asset('assets/plugins/datatable/js/dataTables.responsive.min.js') }}></script>
         {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+
+        <script>
+            function exportData(dataType, exportType) {
+                const urlParams = new URLSearchParams(window.location.search);
+
+                let url = `/dashboard/export/export/${dataType}?export_type=${exportType}`;
+
+                if (urlParams.has('sort')) {
+                    url += `&sort=${urlParams.get('sort')}`;
+                }
+
+                console.log('Export URL:', url);
+                window.location.href = url;
+            }
+        </script>
     @endpush
 </x-app-layout>
--- resources/views/dashboard/pendaftaran/index.blade.php
+++ resources/views/dashboard/pendaftaran/index.blade.php
@@ -4,9 +4,14 @@
             <div class="card-body">
                 <div id="head" class="flex flex-row justify-between">
                     <h2 class="mb-6 text-2xl font-bold card-title">Data Pendaftaran Pasien</h2>
-                    <a href="{{ route('pendaftaran.create') }}" class="btn btn-primary">
-                        <i class="ri-add-line"></i> Tambah data
-                    </a>
+                    <div>
+                        <a href="{{ route('pendaftaran.create') }}" class="btn btn-primary btn-sm">
+                            <i class="ri-add-line"></i> Tambah data
+                        </a>
+                        <button onclick="exportData('pendaftaran', 'pdf')" class="btn btn-outline btn-neutral btn-sm">
+                            <i class="ri-file-pdf-2-line"></i> Export PDF
+                        </button>
+                    </div>
                 </div>
                 {{ $dataTable->table(['class' => 'w-full table table-zebra mt-2 hover:table-auto']) }}
             </div>
@@ -25,5 +30,20 @@
         <script src={{ asset('assets/plugins/datatable/js/dataTables.dataTables.min.js') }}></script>
         <script src={{ asset('assets/plugins/datatable/js/dataTables.responsive.min.js') }}></script>
         {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+
+        <script>
+            function exportData(dataType, exportType) {
+                const urlParams = new URLSearchParams(window.location.search);
+
+                let url = `/dashboard/export/export/${dataType}?export_type=${exportType}`;
+
+                if (urlParams.has('sort')) {
+                    url += `&sort=${urlParams.get('sort')}`;
+                }
+
+                console.log('Export URL:', url);
+                window.location.href = url;
+            }
+        </script>
     @endpush
 </x-app-layout>

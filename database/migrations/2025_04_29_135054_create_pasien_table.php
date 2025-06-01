1ada5c29bb842d13a91047f5e7c60e9d4c8f1019
hawkxc <hawkxc068@gmail.com>
2025-06-01 11:01:37 +0700

    update : update pasien migration

--- database/migrations/2025_04_29_135054_create_pasien_table.php
+++ database/migrations/2025_04_29_135054_create_pasien_table.php
@@ -14,18 +14,18 @@ return new class extends Migration
         Schema::create('pasien', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->string('nik')->nullable(false);
-            $table->string('nama_kk', 50)->nullable(false);
-            $table->string('nama', 50)->nullable(false);
+            $table->string('nik')->nullable(true);
+            $table->string('nama_kk', 50)->nullable(true);
+            $table->string('nama', 50)->nullable(true);
             $table->string('tempat_lahir', 20)->nullable(true);
             $table->date('tanggal_lahir')->nullable(true);
             $table->text('alamat')->nullable(true);
-            $table->enum('agama', ['islam', 'kristen', 'katolik', 'hindu', 'buddha', 'konghucu']);
-            $table->enum('pendidikan', ['belum sekolah', 'sd', 'smp/sltp', 'sma/slta', 'diploma i/ii/iii', 's1/s2/s3', 'lain-lain']);
-            $table->enum('pekerjaan', ['wiraswasta', 'pns', 'ibu rumah tangga', 'pelajar', 'mahasiswa', 'petani', 'pedagang', 'tidak bekerja']);
-            $table->string('penanggung_jawab', 50);
-            $table->enum('golda', ['A', 'B', 'AB', 'O']);
-            $table->string('no_telp', 13);
+            $table->enum('agama', ['islam', 'kristen', 'katolik', 'hindu', 'buddha', 'konghucu'])->nullable(true);
+            $table->enum('pendidikan', ['belum sekolah', 'sd', 'smp/sltp', 'sma/slta', 'diploma i/ii/iii', 's1/s2/s3', 'lain-lain'])->nullable(true);
+            $table->enum('pekerjaan', ['wiraswasta', 'pns', 'ibu rumah tangga', 'pelajar', 'mahasiswa', 'petani', 'pedagang', 'tidak bekerja'])->nullable(true);
+            $table->string('penanggung_jawab', 50)->nullable(true);
+            $table->enum('golda', ['A', 'B', 'AB', 'O'])->nullable(true);
+            $table->string('no_telp', 13)->nullable(true);
             $table->timestamps();
         });
     }
